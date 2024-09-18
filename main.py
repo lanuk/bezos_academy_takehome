bf654d64412665f25155d79f494bd9779d102758
lanuk <lanuk@users.noreply.github.com>
2024-09-18 12:30:13 -0700

    Update main.py
    rewrote using less packages

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import boto3
 from dotenv import load_dotenv
-from io import StringIO
+import json
 import os
-import pandas as pd
 import requests
 
 API_URL = "https://educationdata.urban.org/api/v1/schools/ccd/enrollment/{year}/{grade}/"
@@ -24,10 +23,10 @@ years = [
             2021
         ]
 
-def pull_and_upload_data(year):
-    print(f'Starting to pull data for {year}.')
-    fetch_url = API_URL.format(year=year, grade=GRADE)
-
+def pull_and_upload_data(year, grade):
+    print(f'Starting to pull data for {year}: ', end='')
+    fetch_url = API_URL.format(year=year, grade=grade)
+    #
     try:
         response = requests.get(fetch_url).json()
     except requests.exceptions.RequestException as err:
@@ -35,7 +34,7 @@ def pull_and_upload_data(year):
     else:
         data = response['results']
         record_count = response['count']
-
+    #
     while response['next']:
         try:
             response = requests.get(response['next']).json()
@@ -43,29 +42,24 @@ def pull_and_upload_data(year):
             raise SystemExit(err)
         else:
             data.extend(response['results'])
-    
-    data_df = pd.json_normalize(data)
-    print(f'Successfully pulled data for {year}.')
-
-    # Checking if every downloaded record has been processed
-    assert record_count == len(data_df), f"""ERROR: Record Count Mismatch!\n
-    Downloaded {record_count} records, but processed {len(data_df)}."""
-
-    with StringIO() as csv_buffer:
-        data_df.to_csv(csv_buffer, index=False)
-        s3_response = S3.put_object(
-            Bucket=BUCKET_NAME,
-            Key=f'prek_enrollment_{year}.csv',
-            Body=csv_buffer.getvalue()
-        )
-        http_response = s3_response.get('ResponseMetadata')
-
-        # Verifying that processed data has been uploaded to AWS bucket
-        if http_response.get('HTTPStatusCode') == 200:
-            print(f'Data for {year} uploaded successfully')
-        else:
-            print(f'Data for {year} NOT uploaded successfully')
+    print(f'SUCCESS')
+    js = [json.dumps(i) for i in data]
+    assert record_count == len(data), f"""ERROR: Record Count Mismatch!\n
+    Downloaded {record_count} records, but processed {len(data)}."""
+    # Start upload to S3
+    print(f'Starting to upload data for {year}: ', end='')
+    s3_response = S3.put_object(
+        Bucket=BUCKET_NAME,
+        Key=f'prek_enrollment_{year}.json',
+        Body='[' + ', \n'.join(js) + ']'
+    )
+    http_response = s3_response.get('ResponseMetadata')
+    # Verifying that processed data has been uploaded to AWS bucket
+    if http_response.get('HTTPStatusCode') == 200:
+        print(f'Data for {year} uploaded successfully\n')
+    else:
+        print(f'Data for {year} NOT uploaded successfully\n')
 
 if __name__ == "__main__":
     for y in years:
-        pull_and_upload_data(y)
+        pull_and_upload_data(y, GRADE)
